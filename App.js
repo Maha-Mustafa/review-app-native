8468aaa478353914f326cb488a2707cfd58881f3
Maha Mustafa <mahamustafa.m2@gmail.com>
2021-01-04 18:13:02 +0500

    initial code

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Home from './screens/Home';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
+//register fonts
+const getFonts = () =>{
+  return Font.loadAsync({
+    'nunito-regular' : require('./assets/fonts/Nunito-Regular.ttf'),
+    'nunito-bold' : require('./assets/fonts/Nunito-Bold.ttf')
+  })
+}
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  //checking if the font is loaded 
+  const[fontsLoaded, setFontsLoaded] = useState(false);
+  if(fontsLoaded){
+    return (
+      <Home/>
+    ); 
+  }else{
+    //it will show apploading component
+    return(
+      <AppLoading
+       startAsync={getFonts}
+       onFinish = { () => setFontsLoaded(true)}
+       onError = {()=> console.log('error')}
+      />
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'pink',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'pink',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
